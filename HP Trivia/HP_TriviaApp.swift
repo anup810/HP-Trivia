76c4a62198dd07449f1be851097168947e84d0e6
Anup Saud <anupsaud10@gmail.com>
2024-08-15 20:32:22 -0400

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Anup Saud on 2024-08-15.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
